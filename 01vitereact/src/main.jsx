2e77c72a4082ec1a74104e3a825c9bfa0fb54e77
Ritesh Khatri <riteshkhatri1512@gmail.com>
2024-02-04 21:39:27 +0530

    Create a react element

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import App from './App.jsx';
+import './index.css';
+
+// Creating an element using apis in React library
+// Properties/ attributes
+// , text content 
+// type of tag
+
+const someMoreText = "Created through React and Vit"
+
+const reactElementToVisitGoogle = React.createElement(
+  'a',
+  { href: 'https://google.com', target: '_blank' },
+  'Hey, Click here to visit Google homepage',
+
+);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+  reactElementToVisitGoogle
 )
