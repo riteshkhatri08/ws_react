1834288f79c876c3708eeee22eb84211bb2229b5
Ritesh Khatri <riteshkhatri1512@gmail.com>
2024-02-18 02:58:23 +0530

    Added jsx structure and fset useState on required fields

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -1,8 +1,61 @@
+import { useState } from "react";
 
 function App() {
+  const [password, setPassword] = useState("Password");
+  const [passLength, setPassLength] = useState(8);
+  const [includeNumbers, setIncludeNumbers] = useState(false);
+  const [includeSpecialCharacters, setIncludeSpecialCharacters] =
+    useState(false);
   return (
     <>
       <h1 className="text-center text-4xl text-white">Password Generator</h1>
+
+      <div className="text-white bg-fuchsia-900 px-4 py-4 flex flex-wrap justify-center">
+        <div className="rounded-xl text-white bg-orange-600 justify-center  px-4 py-4 flex flex-wrap align-middle flex-col">
+          <div className="flex flex-wrap flex-row ">
+            <input
+              type="text"
+              value={password}
+              className="bg-gray-700 px-2 py-2 rounded-s-md flex-grow"
+              readOnly
+            />
+            <button className="bg-blue-500 px-2 py-2 rounded-e-md">COPY</button>
+          </div>
+          <div className="flex flex-row flex-wrap gap-x-5">
+            <label>
+              <input
+                type="range"
+                alt="length"
+                min="6"
+                max="20"
+                value={passLength}
+                onChange={(e) => setPassLength(e.target.value)}
+              />
+              Length({passLength})
+            </label>
+
+            <label>
+              <input
+                name="includeNumbers"
+                type="checkbox"
+                checked={includeNumbers}
+                onChange={(e) => setIncludeNumbers(e.target.checked)}
+              />
+              Numbers
+            </label>
+
+            <label>
+              <input
+                name="includeSpecialCharacters"
+                type="checkbox"
+                checked={includeSpecialCharacters}
+                onChange={(e) => setIncludeSpecialCharacters(e.target.checked)}
+              />
+              Special Characters
+            </label>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
