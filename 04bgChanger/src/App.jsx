054cf95af3b2a20785865d10331a35f3e8b5d231
Ritesh Khatri <riteshkhatri1512@gmail.com>
2024-02-17 19:43:19 +0530

    Added component for color changes

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -1,10 +1,45 @@
+import { useState } from "react";
+
 function App() {
+  const [color, setColor] = useState("olive");
   return (
-    <>
-      <h1 className="text-3xl font-bold underline text-fuchsia-400">
-        Hello world!
-      </h1>
-    </>
+    <div
+      className="w-full h-screen duration-200"
+      style={{ backgroundColor: color }}
+    >
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg  bg-white px-3 py-2 rounded-2xl">
+          <button
+            className="outline-none px-4 py-1 rounded-full bottom-12 text-white"
+            style={{ backgroundColor: "red" }}
+            onClick={() => setColor("red")}
+          >
+            Red
+          </button>
+          <button
+            className="outline-none px-4 py-1 rounded-full text-white"
+            style={{ backgroundColor: "green" }}
+            onClick={() => setColor("green")}
+          >
+            Green
+          </button>
+          <button
+            className="outline-none px-4 py-1 rounded-full text-white"
+            style={{ backgroundColor: "blue" }}
+            onClick={() => setColor("blue")}
+          >
+            Blue
+          </button>
+          <button
+            className="outline-none px-4 py-1 rounded-full text-white"
+            style={{ backgroundColor: "orange" }}
+            onClick={() => setColor("orange")}
+          >
+            Orange
+          </button>
+        </div>
+      </div>
+    </div>
   );
 }
 
