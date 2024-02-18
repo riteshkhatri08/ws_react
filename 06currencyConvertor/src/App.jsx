c5d4e2b35bd1967123078603f59c6f6fb23bd96e
Ritesh Khatri <riteshkhatri1512@gmail.com>
2024-02-18 14:02:49 +0530

    Added Input Box without any values in App Component

--- 06currencyConvertor/src/App.jsx
+++ 06currencyConvertor/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { InputBox } from "./components";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -8,6 +9,7 @@ function App() {
       <h1 className="text-white text-3xl bg-orange-500 text-center">
         Currency Convertor
       </h1>
+      <InputBox />
     </>
   );
 }
