17bccf44866e01089791fa8e710d88e5cdd390ff
riteshkhatri08 <riteshkhatri1512@gmail.com>
2025-11-17 04:39:13 +0530

    minor refactor

--- react_revisit/react-recap-1/src/Common.ts
+++ react_revisit/react-recap-1/src/Common.ts
@@ -10,3 +10,7 @@ export const createTask = (value: string, isComplete?: boolean): Task => {
     return { id: uuidv4(), value: value, isComplete: isComplete || false }
 }
 
+// type for redux store state
+export interface TodoListState {
+    tasks: Task[]
+}
--- react_revisit/react-recap-1/src/store.ts
+++ react_revisit/react-recap-1/src/store.ts
@@ -1,9 +1,7 @@
-import type { Task } from "./Common";
+import type { Task, TodoListState } from "./Common";
 import { configureStore, createSlice, type PayloadAction } from '@reduxjs/toolkit'
 
-interface TodoListState {
-    tasks: Task[]
-}
+
 const initialState: TodoListState = { tasks: [] };
 
 // create a slice for Task list
@@ -13,19 +11,19 @@ const todoListSlice = createSlice(
         initialState: initialState,
         reducers: {
             // Add task reducer
-            addNewTask: (state, action: PayloadAction<Task>) => {
+            addNewTask: (state: TodoListState, action: PayloadAction<Task>) => {
                 state.tasks = [action.payload, ...state.tasks];
             },
             // delete task reducer
-            removeTask: (state, action: PayloadAction<string>) => {
+            removeTask: (state: TodoListState, action: PayloadAction<string>) => {
                 state.tasks = state.tasks.filter((task: Task) => task.id === action.payload ? false : true);
             },
             // clear all tasks reducer
-            clearAllTasks: (state) => {
+            clearAllTasks: (state: TodoListState) => {
                 state.tasks = initialState.tasks
             },
             // mark a task as completed , reducer
-            completeTask: (state, action: PayloadAction<string>) => {
+            completeTask: (state: TodoListState, action: PayloadAction<string>) => {
                 state.tasks = state.tasks.map(task => { return task.id === action.payload ? { ...task, isComplete: true } : task; });
             }
         }
@@ -33,7 +31,7 @@ const todoListSlice = createSlice(
     }
 );
 
-export const {addNewTask, removeTask, clearAllTasks, completeTask} = todoListSlice.actions;
+export const { addNewTask, removeTask, clearAllTasks, completeTask } = todoListSlice.actions;
 
 // Create a store
 // Reducer takes in an Action and takes in a previous state then it will return a new updated stat
